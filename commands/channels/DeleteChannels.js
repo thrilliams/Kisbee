bc995040683834b2cf0f1edf9323c731a107df53
june <juniper@aethre.co>
2020-08-16 21:48:05 -0700

    implement channels

---
+++ commands/channels/DeleteChannels.js
@@ -0,0 +1,34 @@
+const { Command } = require('discord.js-commando');
+const primeTimeTable = require('../../api/primeTimeTable.js');
+
+module.exports = class CreateRoles extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'deletechannels',
+            group: 'channels',
+            memberName: 'delete',
+            description: 'Deletes all subject channels. Requires the Manage Channels permission.',
+            guildOnly: true,
+            userPermissions: [ 'MANAGE_CHANNELS' ]
+        });
+    }
+
+    async run(msg, args) {
+        let channels = msg.guild.settings.get('subjectChannels');
+        
+        if (channels === undefined) return;
+
+        for (let channel in channels) {
+            channel = await msg.guild.channels.resolve(channel);
+            await channel.delete();
+            msg.guild.settings.remove('subjectChannel.' + subject.id);
+        }
+
+        let categoryId = msg.guild.settings.get('subjectChannel');
+        if (categoryId !== undefined) {
+            let channel = await msg.guild.channels.resolve(categoryId);
+            await channel.delete();
+            msg.guild.settings.remove('subjectChannel');
+        }
+    }
+}
