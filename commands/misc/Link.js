1b0a79de39d50e7c5f989988ece64d9840049626
june <juniper@aethre.co>
2020-08-18 23:42:02 -0700

    implement links

---
+++ commands/misc/Link.js
@@ -0,0 +1,38 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class Link extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'link',
+            aliases: [ 'info', 'links' ],
+            group: 'misc',
+            memberName: 'link',
+            description: 'Retrieve helpful information on a link. Returns all links when called with no arguments.',
+            args: [{
+                key: 'name',
+                type: 'string',
+                prompt: 'Please specify the name of an item you want to view.',
+                default: ''
+            }]
+        });
+    }
+    
+	async run(msg, args) {
+        if (args.name && args.name !== 'list') {
+            let item = msg.guild.settings.get('links.' + args.name.toLowerCase(), args.value);
+            if (!item) return msg.channel.send('Kisbee could not find an item with that name');
+            let embed = new MessageEmbed()
+                .addField(args.name.charAt(0).toUpperCase() + args.name.slice(1), item);
+            msg.channel.send(embed);
+        } else {
+            let items = msg.guild.settings.get('links');
+            if (!items) return msg.channel.send('There are no items saved with Kisbee.');
+            let embed = new MessageEmbed();
+            for (let item in items) {
+                embed.addField(item.charAt(0).toUpperCase() + item.slice(1), items[item]);
+            }
+            msg.channel.send(embed);
+        }
+	}
+}
---
+++ commands/misc/SetLink.js
@@ -0,0 +1,31 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class SetLink extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'setlink',
+            aliases: [ 'setinfo' ],
+            group: 'misc',
+            memberName: 'setlink',
+            description: 'Set helpful information.',
+            args: [
+                {
+                    key: 'name',
+                    type: 'string',
+                    prompt: 'Please specify the name of an item you want to set.'
+                },
+                {
+                    key: 'value',
+                    type: 'string',
+                    prompt: 'Please specify what you want the value of the item to be.'
+                }
+            ]
+        });
+    }
+    
+	async run(msg, args) {
+        msg.guild.settings.set('links.' + args.name.toLowerCase(), args.value);
+        msg.channel.send(new MessageEmbed().addField(args.name, args.value));
+	}
+}
