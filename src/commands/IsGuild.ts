dd18d6ec7edb05bb65c982ca2e05b7acd67bb63d
june <juniper@aethre.co>
2021-09-06 22:19:11 -0700

    slightly alter IsGuild so it doesn't crash

--- src/commands/IsGuild.ts
+++ src/commands/IsGuild.ts
@@ -1,11 +1,9 @@
+import { CommandInteraction } from 'discord.js';
 import { GuardFunction, ArgsOf } from 'discordx';
 
-export const IsGuild: GuardFunction<ArgsOf<'interactionCreate'>> = async (
-    [interaction],
-    client,
-    next
-) => {
-    if (interaction.guild !== null) {
-        await next();
+export const IsGuild: GuardFunction<CommandInteraction> =
+    async (interaction, client, next) => {
+        if (interaction.guild !== null) {
+            await next();
+        }
     }
-};
