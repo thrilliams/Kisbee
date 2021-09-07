a0c9c4f74210c16c944258e063ca81e140f47440
Soren <soren@aethre.co>
2021-09-06 20:38:43 -0700

    finish functional draft of /channel verify

--- src/commands/Channel.ts
+++ src/commands/Channel.ts
@@ -1,6 +1,8 @@
 import { Discord, Permission, Slash, SlashGroup, SlashOption } from 'discordx';
-import { TextChannel, CommandInteraction, MessageAttachment } from 'discord.js';
-import * as fetchAll from 'discord-fetch-all';
+import { TextChannel, CommandInteraction, MessageAttachment, Collection } from 'discord.js';
+import { messages as fetchMessages } from 'discord-fetch-all';
+import PrimeTimeTable from '../lib/PrimeTimeTable';
+import { MessageEmbed } from 'discordx/node_modules/discord.js';
 
 @Discord()
 @SlashGroup('channel', 'Commands for manipulating subject channels.')
@@ -14,7 +16,7 @@ abstract class Channel {
         await interaction.reply('Archiving, please wait...');
 
         const channel = interaction.channel as TextChannel;
-        const messages = await fetchAll.messages(channel, {
+        const messages = await fetchMessages(channel, {
             userOnly: false,
             botOnly: false,
             reverseArray: true,
@@ -36,5 +38,71 @@ abstract class Channel {
         } else {
             interaction.editReply({ content: 'Channel archived.', files: [attachment] })
         }
+    }
+
+    @Permission({ id: '294625075934527495', type: 'USER', permission: true })
+    @Slash('verify', { description: 'Ensures channels exist for each applicable subject.', defaultPermission: false })
+    async verify(
+        @SlashOption('min', { description: 'Minimum students in a class for it be considered.' }) min: number,
+        @SlashOption('max', { description: 'Maximum students in a class for it be considered.' }) max: number,
+        interaction: CommandInteraction
+    ) {
+        let embed = new MessageEmbed();
+        let success = true;
+        embed.setTitle('Verifying channels, please wait...')
+            .addField('Channel names:', '...')
+            .addField('Channel permissions:', '...')
+        await interaction.reply({ embeds: [embed] });
+
+        let table = new PrimeTimeTable();
+        let groups = table.filterSections(min, max);
+
+        let channelStatuses: Collection<string, TextChannel> = new Collection();
+        groups.forEach(group => {
+            let pattern = /(\w+)/g;
+            let result = pattern.exec(group.name);
+            let string = '';
+
+            while (result !== null) {
+                string += `-${result[0].toLowerCase()}`;
+                result = pattern.exec(group.name);
+            }
+
+            channelStatuses.set(string.slice(1), undefined);
+        });
+
+        let channels = await interaction.guild.channels.fetch();
+        channels = channels.filter(channel => channel.type === "GUILD_TEXT");
+
+        for (let name of channelStatuses.keys()) {
+            // TODO: Fuzzy name search
+            let channel = channels.find(channel => channel.name === name);
+            if (channel !== undefined && channel.type === "GUILD_TEXT") {
+                channelStatuses.set(name, channel);
+            }
+        }
+
+        let totalFound = channelStatuses.reduce((total, channel) => total += channel !== undefined ? 1 : 0, 0);
+        embed.fields.find(field => field.name === 'Channel names:').value = `${totalFound}/${channelStatuses.size} found.`;
+        if (totalFound < channelStatuses.size) {
+            embed.addField('⚠️ Missing channels:', [...channelStatuses.filter(channel => channel === undefined).keys()].map(name => `#${name}`).join('\n'));
+            success = false;
+        }
+        channelStatuses = channelStatuses.filter(channel => channel !== undefined);
+        await interaction.editReply({ embeds: [embed] });
+
+        function checkPerms(channel: TextChannel) {
+            // TODO: Correct permission check
+            return channel.permissionsFor(interaction.guildId).has('SEND_MESSAGES');
+        }
+
+        let totalCorrect = channelStatuses.reduce((total, channel) => total += checkPerms(channel) ? 1 : 0, 0);
+        embed.fields.find(field => field.name === 'Channel permissions:').value = `${totalCorrect}/${channelStatuses.size} correct.`;
+        if (totalFound < channelStatuses.size) {
+            embed.addField('⚠️ Incorrect permissions:', [...channelStatuses.filter(channel => !checkPerms(channel)).keys()].map(name => `#${name}`).join('\n'));
+            success = false;
+        }
+        embed.setTitle(success ? 'Subject channels successfully verified.' : '⚠️ Subject channel verification failed! Please adress issues below.');
+        await interaction.editReply({ embeds: [embed] });
     }
 }
